dbdaa8696b484180d31b8776a361a460b00b55ee
trentyy <trent.y.yang@gmail.com>
2024-03-07 15:45:24 +0800

    update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import time, asyncio, random
+import asyncio, random
 from pymodbus.client import ModbusTcpClient
 
-
+# config settings
 host = 'localhost'
-regs = [40001 + i*2 for i in range(0, 6)]
+slave_id = 1
+regs = [1 + i*2 for i in range(0, 6)]
+
+
 stations = {}
 for reg in regs:
     stations[reg] = {0:0, 1:0, 2:0}
@@ -11,10 +14,16 @@ for reg in regs:
 async def station(client, state_data, reg):
     while True:
         await asyncio.sleep(1)
-        result = client.read_holding_registers(reg, 1)
+        result = client.read_holding_registers(reg, 1, slave_id)
+        result = result.registers[0]
         # result = random.randint(0,2)
         state_data[result] = state_data[result] + 1
-        print(reg, state_data)
+async def print_result():
+    while True:
+        for reg in regs:
+            print(40000+reg, stations[reg])
+        print("="*16)
+        await asyncio.sleep(1)
 
 if __name__ == "__main__":
     client = ModbusTcpClient(host)
@@ -23,4 +32,5 @@ if __name__ == "__main__":
     tasks = []
     for _ in regs:
         tasks.append(loop.create_task(station(client, stations[_], _)))
+    tasks.append(loop.create_task(print_result()))
     loop.run_until_complete(asyncio.wait(tasks))
